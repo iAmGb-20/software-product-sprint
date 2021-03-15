9c49150b1b7d81bdde003528799f5ea85a59c68c
iAmGb-20 <gchille@sps-program.com>
2021-03-15 05:19:18 +0000

    some week-3 done

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -39,3 +39,41 @@ function addRandomQuote()
     const quoteContainer = document.getElementById('quote-container');
     quoteContainer.innerText = quote;
 }
+
+/**
+ * Creates an image given a source and appends it to the parent.
+ * @param {URL} src Source of image
+ * @param {Document Element} parent Element to append image to 
+ */
+function appendImage(src, parent) {
+    let img = document.createElement("img");
+    img.src = src;
+    parent.appendChild(img);
+}
+
+function appendText(txt, parent) {
+    let p = document.createElement("p");
+    p.innerText = txt;
+    parent.appendChild(p);
+}
+/**
+ * Gets a list of images from the server,
+ * trying the JSON method of listing the files
+ */
+async function fetchImages()
+{
+    const responseFromServer = await fetch('/list-images');
+    const jsonFromServ = await responseFromServer.json();
+    console.log(`Did the JSON return as an actual object? ${jsonFromServ != null}`);
+    console.log(jsonFromServ);
+    const imageContainer = document.getElementById("images");
+    const doc = document;
+    if (jsonFromServ.length > 0) {
+        jsonFromServ.forEach(function(src) {
+            appendImage(src.mediaLink, imageContainer);
+            appendText(src.tag, imageContainer);
+        });
+    } else {
+        imageContainer.innerText = "No images yet. :(";
+    }
+}
