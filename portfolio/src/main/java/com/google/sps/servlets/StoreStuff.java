83ccb933f74c588042f46fb4e6e6f4f7d7f56035
iAmGb-20 <gchille@sps-program.com>
2021-04-01 00:35:15 +0000

    added datastore to webpage

--- portfolio/src/main/java/com/google/sps/servlets/StoreStuff.java
+++ portfolio/src/main/java/com/google/sps/servlets/StoreStuff.java
@@ -17,15 +17,22 @@ package com.google.sps.servlets;
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
 import com.google.cloud.datastore.FullEntity;
 import com.google.cloud.datastore.KeyFactory;
+import com.google.cloud.datastore.StructuredQuery.OrderBy;
 import java.io.IOException;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+import com.google.sps.data.Comments;
+import java.util.List;
+import java.util.Date;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//import org.jsoup.Jsoup;
-//import org.jsoup.safety.Whitelist;
+
 
 /** Servlet responsible for creating new tasks. */
 @WebServlet("/upload")
@@ -34,15 +41,12 @@ public class StoreStuff extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException 
   {
+      
     String userString = getParameter(request, "message", "");    
-    //String title = Jsoup.clean(request.getParameter("title"), Whitelist.none());
     long timestamp = System.currentTimeMillis();
 
-    
-    // Redirect back to the HTML page.
-    //response.sendRedirect("/index.html");
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService(); //get the instance of the Datastore class
-    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Task"); //creates a keyFactory with a kind called "Task" and the name keyFactory
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Comments"); //creates a keyFactory with a kind called "Task" and the name keyFactory
     FullEntity taskEntity =
         Entity.newBuilder(keyFactory.newKey()) //give a key to the Entity
             .set("comment", userString)//set the associated attributes of the key (values)
@@ -54,6 +58,43 @@ public class StoreStuff extends HttpServlet {
     response.sendRedirect("/index.html");
   }
 
+  //load data from the "contact me" form
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException 
+  {
+    //create an instance of datastore to use
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+
+    //create a query instance to use
+    Query<Entity> query =
+        Query.newEntityQueryBuilder().setKind("Comments").setOrderBy(OrderBy.desc("timestamp")).build();
+    QueryResults<Entity> results = datastore.run(query);
+
+    List<Comments> messages = new ArrayList<>();//initializes a list of comments
+    while (results.hasNext()) 
+    {
+      Entity entity = results.next();
+
+      long id = entity.getKey().getId();
+      String comment = entity.getString("comment");
+      long timestamp = entity.getLong("timestamp");
+      Date day = new Date(timestamp);
+      Comments c = new Comments(comment, id, day.toString());
+      messages.add(c);
+    }
+    String json = convertToJson(messages);
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+
+  }//iterate through the stuff in datastore, extract properties and save them in Comments..
+  //add these to list after that
+  
+  //helper function to convert to Json
+  private String convertToJson(List<Comments> sample)
+  {
+    Gson gson = new Gson();
+    String json = gson.toJson(sample);
+    return json;
+  }
   private String getParameter(HttpServletRequest request, String name, String defaultValue) {
     String value = request.getParameter(name);
     if (value == null) 
--- portfolio/src/main/webapp/script1.js
+++ portfolio/src/main/webapp/script1.js
@@ -6,3 +6,36 @@ async function showServerString() {
   const stringContainer = document.getElementById('string-container');
   stringContainer.innerText = textFromResponse;
 }
+
+async function showMessages()
+{
+    const responseFromServer = await fetch('/upload');
+    const textFromResponse = await responseFromServer.text();
+    const stringContainer = document.getElementById('message-container');
+    stringContainer.innerText = textFromResponse;
+}
+
+
+/** Fetches tasks from the server and adds them to the DOM. */
+function loadMessages() {
+  fetch('/upload').then(response => response.json()).then((messages) => {
+    const messagesListEl = document.getElementById('task-list');
+    messages.forEach((message) => {
+      const messageEl = document.createElement('li');
+      messageEl.className = "Message";
+      messageEl.innerHTML ='';
+      messageEl.appendChild(createListElement(message.comment));
+      messageEl.appendChild(createListElement("---"));
+      messageEl.appendChild(createListElement(message.timestamp));
+      messageEl.appendChild(document.createElement('hr'));
+      messagesListEl.appendChild(messageEl);
+    })
+  });
+}
+
+/** Creates an <li> element containing text. */
+function createListElement(text) {
+  const liElement = document.createElement('li');
+  liElement.innerText = text;
+  return liElement;
+}
